52007ed1dd94e69bf10bbf3d1a42a0b0659d39d6
Shannon <shannon@Mirkwood.local>
2018-03-21 14:51:36 -0500

    update

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Shannon on 3/20/18.
+//  Copyright © 2018 Shannon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topMemeText: String!
+    var bottomMemeText: String!
+    var originalImage: UIImage!
+    var finishedMeme: UIImage!
+}
---
+++ MemeMe/SavedMemes.swift
@@ -0,0 +1,13 @@
+//
+//  SavedMemes.swift
+//  MemeMe
+//
+//  Created by Shannon on 3/20/18.
+//  Copyright © 2018 Shannon. All rights reserved.
+//
+
+import Foundation
+
+struct SavedMemes {
+    static var allSavedMemes = [Meme]()
+}
